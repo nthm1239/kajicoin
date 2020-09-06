b1ddfc490c26d8fbff55ecf88fbb68aabc77dae9
nthm1239 <wmkfkkt.dev@gmail.com>
2020-09-06 14:58:14 +0900

    Home画面を作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,53 @@
 <template>
-  <hello-world />
+  <div class="Home">
+    <v-container fluid >
+      <v-row>
+        <v-col cols="12">
+          <v-btn
+            color="accent"
+            block
+          >
+            <v-icon>mdi-home</v-icon>
+          </v-btn>
+        </v-col>
+
+        <v-col cols="4">
+          <v-btn
+            color="accent"
+            block
+          >
+            <v-icon>mdi-currency-cny</v-icon>
+          </v-btn>
+        </v-col>
+
+        <v-col cols="4">
+          <v-btn
+            color="accent"
+            block
+          >
+            <v-icon>mdi-gift</v-icon>
+          </v-btn>
+        </v-col>
+
+        <v-col cols="4">
+          <v-btn
+            color="accent"
+            block
+          >
+            <v-icon>mdi-washing-machine</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'Home',
 
     components: {
-      HelloWorld,
     },
   }
 </script>
