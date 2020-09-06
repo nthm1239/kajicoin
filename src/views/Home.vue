62bc84ea7b1e523fded4a1b5efa39d49e8f657e9
nthm1239 <wmkfkkt.dev@gmail.com>
2020-09-07 00:51:01 +0900

    トップ画面のレイアウトを更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,40 +3,17 @@
     <v-container fluid >
       <v-row>
         <v-col cols="12">
-          <v-btn
-            color="accent"
-            block
-          >
-            <v-icon>mdi-home</v-icon>
-          </v-btn>
-        </v-col>
-
-        <v-col cols="4">
-          <v-btn
-            color="accent"
-            block
-          >
-            <v-icon>mdi-currency-cny</v-icon>
-          </v-btn>
-        </v-col>
-
-        <v-col cols="4">
-          <v-btn
-            color="accent"
-            block
-          >
-            <v-icon>mdi-gift</v-icon>
-          </v-btn>
-        </v-col>
-
-        <v-col cols="4">
-          <v-btn
-            color="accent"
-            block
-            to="/housework"
-          >
-            <v-icon>mdi-washing-machine</v-icon>
-          </v-btn>
+          <v-row v-resize="onResize" :style="style" >
+            <v-col cols="6" v-for="menu in menus" v-bind:key="menu.key">
+              <v-btn
+                color="accent"
+                block
+                height='100%'
+              >
+                <v-icon :size="iconSize">{{menu.icon}}</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-container>
@@ -47,7 +24,36 @@
 
   export default {
     name: 'Home',
+    data: () => ({
+      menus: [
+        { title: 'home', icon: 'mdi-home' },
+        { title: 'currency', icon: 'mdi-currency-cny' },
+        { title: 'gift', icon: 'mdi-gift' },
+        { title: 'kaji', icon: 'mdi-washing-machine'},
+      ],
+      windowSize: {
+        x: 0,
+        y: 0,
+      },
+      iconSize: 0 
+    }),
 
+    mounted () {
+      this.onResize()
+    },
+
+    computed: {
+      style () {
+        return 'height: ' + this.windowSize.y * 0.8 + 'px;'
+      }
+    },
+
+    methods: {
+      onResize () {
+        this.windowSize = { x: window.innerWidth, y: window.innerHeight }
+        this.iconSize = window.innerHeight * 0.1
+      },
+    },
     components: {
     },
   }
